864d3de1a0a99168785f676b3af9e99d8073d727
Arj <arjun.dinesh@msam.christuniversity.in>
2023-12-01 12:46:14 +0530

    commit

--- JAVAClass/JavaPractical/Lab2/ShareTrade.java
+++ JAVAClass/JavaPractical/Lab2/ShareTrade.java
@@ -5,8 +5,8 @@ public class ShareTrade {
 	public static void main(String[] args)   {  
 		Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter the stock prices: ");
-        String[] input = scanner.nextLine().split(" ");
+        System.out.println("Enter the stock prices >>> ");
+        String[] input = scanner.nextLine().split(",");
 
         int n = input.length;
         int price[] = new int[n];
@@ -16,10 +16,16 @@ public class ShareTrade {
         }
         System.out.println(maxProfit(price, n));
 	}
+    static int maxProfit(int price[], int n) {
+        int maxProfit = 0;
+        int profitAfterBuy = Integer.MIN_VALUE;
 
+        for (int i = 0; i < n; i++) {
+            maxProfit = Math.max(maxProfit, price[i] + profitAfterBuy);
+            profitAfterBuy = Math.max(profitAfterBuy, -price[i]);
+        }
 
-
-
-
+        return maxProfit;
+    }
 
 }
