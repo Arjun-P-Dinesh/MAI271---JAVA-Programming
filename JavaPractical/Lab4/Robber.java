8f4a68a617eca6aa378f8eaac220574684b9d033
Arjun-P-Dinesh <aarjun.dinesh@msam.christuniversity.in>
2023-12-09 12:28:34 +0530

    commit

--- JavaPractical/Lab4/Robber.java
+++ JavaPractical/Lab4/Robber.java
@@ -3,10 +3,21 @@ package JavaPractical.Lab4;
 import java.util.*;
 
 //creating abstract class named Robber
-abstract class Robber{
-    
+public abstract class Robber{
+    public void RobbingClass() {
+        System.out.println("MScAI&ML");
+    }
+    public void MachineLearning() {
+        System.out.println("I love MachineLearning.");
+    }
 }
 class JAVAProfessionalRobber extends Robber{
 
 }
-
+class Main{
+    public static void main(String[] args) {
+    JAVAProfessionalRobber Robber = new JAVAProfessionalRobber();
+    Robber.RobbingClass();
+    Robber.MachineLearning();
+    }
+}
