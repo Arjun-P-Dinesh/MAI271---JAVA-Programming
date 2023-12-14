3324b83869fd3f834512305566bbfd1d532712b8
Arjun-P-Dinesh <aarjun.dinesh@msam.christuniversity.in>
2023-12-14 12:07:51 +0530

    commit

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # MAI271---JAVA-Programming
 
-<link href="https://languages.abranhe.com/logos.css" rel="stylesheet">
+<a href="https://languages.abranhe.com/logos.css" rel="stylesheet">
 
 <i class="programming lang-java"></i>
---
+++ javaTheory/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //print
+        System.out.println("Hello World!");
+
+        //input
+        System.out.println("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Hello " + name + "!");
+    }
+}
